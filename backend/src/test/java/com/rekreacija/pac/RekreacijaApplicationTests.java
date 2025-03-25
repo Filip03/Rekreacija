db0bf9dd74755f9f75cb83a8e7cafe68ab62d5a5
Filip <filipzejak2@gmail.com>
2025-03-25 11:25:15 +0100

    inicijalizovan backend

---
+++ backend/src/test/java/com/rekreacija/pac/RekreacijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rekreacija.pac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekreacijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
