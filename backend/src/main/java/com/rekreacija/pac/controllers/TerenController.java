36d4812fddefdf9557850aa29110f748238113b9
Filip <filipzejak2@gmail.com>
2025-04-03 17:43:07 +0200

    CRUD for Teren is done

---
+++ backend/src/main/java/com/rekreacija/pac/controllers/TerenController.java
@@ -0,0 +1,41 @@
+package com.rekreacija.pac.controllers;
+
+import com.rekreacija.pac.models.Teren;
+import com.rekreacija.pac.services.TerenService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teren")
+public class TerenController {
+
+    private TerenService terenService;
+
+    public TerenController(TerenService terenService) { this.terenService = terenService; }
+
+    @GetMapping()
+    public List<Teren> getAllTeren(){
+        return this.terenService.getAllteren();
+    }
+
+    @GetMapping("/{id}")
+    public Teren getTerenById(@PathVariable("id") int id){
+        return this.terenService.getTerenById(id);
+    }
+
+    @PostMapping()
+    public int insertTeren(@RequestBody Teren teren){
+        return this.terenService.insertTeren(teren);
+    }
+
+    @PutMapping("/{id}")
+    public int updateTeren(@RequestBody Teren teren, @PathVariable("id") int id){
+        return this.terenService.updateTeren(teren, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public int deleteTeren(@PathVariable("id") int id){
+        return this.terenService.deleteTeren(id);
+    }
+}
