b3b9c37b96454e695cfa9b12e3d660526d0e9740
Filip <filipzejak2@gmail.com>
2025-04-14 13:50:52 +0200

    added header and footer

---
+++ frontend/rekreacija/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/helpers/header/header.component';
+import { FooterComponent } from './components/helpers/footer/footer.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import {NgOptimizedImage} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
