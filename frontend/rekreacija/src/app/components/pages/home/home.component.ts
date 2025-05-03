de526317a119b45beb74a83c851bece3536302e3
Filip <filipzejak2@gmail.com>
2025-05-04 01:21:31 +0200

    created landing page

--- frontend/rekreacija/src/app/components/pages/home/home.component.ts
+++ frontend/rekreacija/src/app/components/pages/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  ngAfterViewInit(): void {
+    const ctaButton = document.getElementById("ctaButton");
+    const hero = document.querySelector(".hero");
 
+    window.addEventListener("scroll", () => {
+      if (!ctaButton || !hero) return;
+
+      const heroBottom = hero.getBoundingClientRect().bottom;
+
+      if (heroBottom <= 100) {
+        ctaButton.classList.add("sticky");
+      } else {
+        ctaButton.classList.remove("sticky");
+      }
+    });
+  }
 }
