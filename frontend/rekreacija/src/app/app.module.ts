d7990059b26a156514b998a175466c8504c0e857
Filip <filipzejak2@gmail.com>
2025-05-04 19:06:18 +0200

    finished about page

--- frontend/rekreacija/src/app/app.module.ts
+++ frontend/rekreacija/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/helpers/header/header.component';
 import { FooterComponent } from './components/helpers/footer/footer.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { RegisterComponent } from './components/pages/register/register.component';
-import {NgOptimizedImage} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from "@angular/common/http";
-import {authInterceptor} from "./interceptors/auth.interceptor";
+import { NgOptimizedImage, CommonModule} from "@angular/common";
+import { FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from "@angular/common/http";
+import { authInterceptor} from "./interceptors/auth.interceptor";
 import { ObavjestenjaComponent } from './components/pages/obavjestenja/obavjestenja.component';
 
 @NgModule({
@@ -29,7 +28,8 @@ import { ObavjestenjaComponent } from './components/pages/obavjestenja/obavjeste
             AppRoutingModule,
             NgOptimizedImage,
             FormsModule,
-            BrowserAnimationsModule
+            BrowserAnimationsModule,
+            CommonModule
             ],
   providers: [provideHttpClient(withInterceptors([
               authInterceptor
--- frontend/rekreacija/src/app/components/pages/about/about.component.ts
+++ frontend/rekreacija/src/app/components/pages/about/about.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {interval, Subscription} from "rxjs";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit, OnDestroy {
+  currentSlide = 0;
+  slides = [
+    { src: 'assets/kosarka_termin.jpg', alt: 'Rezervacije', title: 'Rezervacije', description: 'Rezervišite teren za omiljeni sport u samo par klikova.' },
+    { src: 'assets/termin.png', alt: 'Timovi', title: 'Kreirajte timove', description: 'Povežite se sa prijateljima i organizujte zajedničke termine.' },
+    { src: 'assets/telefon.png', alt: 'Statistika', title: 'Budite u koraku sa svim novostima', description: 'Dozvolite push notifikacije da Vam ništa ne bi promaklo.' }
+  ];
+  private autoSlideSubscription: Subscription | undefined;
 
+  ngOnInit(): void {
+    this.showSlide(this.currentSlide);
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy(): void {
+    this.stopAutoSlide();
+  }
+
+  showSlide(index: number): void {
+    this.currentSlide = index;
+  }
+
+  changeSlide(direction: number): void {
+    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+    this.showSlide(this.currentSlide);
+  }
+
+  startAutoSlide(): void {
+    this.autoSlideSubscription = interval(5000).subscribe(() => {
+      this.changeSlide(1);
+    });
+  }
+
+  stopAutoSlide(): void {
+    if (this.autoSlideSubscription) {
+      this.autoSlideSubscription.unsubscribe();
+    }
+  }
+
+  resetAutoSlide(): void {
+    this.stopAutoSlide();
+    this.startAutoSlide();
+  }
 }
