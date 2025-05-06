6a05734216ccb48e8a6346acf6a832f77b11abbc
Filip <filipzejak2@gmail.com>
2025-05-06 13:32:06 +0200

    new database schema

--- database/rekreacija_database_for_import.sql
+++ database/rekreacija_database_for_import.sql
@@ -38,7 +38,7 @@ CREATE TABLE `ekipa` (
 
 LOCK TABLES `ekipa` WRITE;
 /*!40000 ALTER TABLE `ekipa` DISABLE KEYS */;
-INSERT INTO `ekipa` VALUES (2,'Gardisti',NULL),(3,'Pro Conto',NULL);
+INSERT INTO `ekipa` VALUES (1,'Stari Aerodrom',4.7),(2,'Zabjelo',4.3),(3,'City Kvart',NULL);
 /*!40000 ALTER TABLE `ekipa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +66,7 @@ CREATE TABLE `korisnik` (
   KEY `tip_korisnika` (`type_id`),
   CONSTRAINT `korisnik_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `ekipa` (`id`),
   CONSTRAINT `tip_korisnika` FOREIGN KEY (`type_id`) REFERENCES `tip` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +75,7 @@ CREATE TABLE `korisnik` (
 
 LOCK TABLES `korisnik` WRITE;
 /*!40000 ALTER TABLE `korisnik` DISABLE KEYS */;
-INSERT INTO `korisnik` VALUES (5,'Marko','Marković','marko@example.com','{bcrypt}$2a$10$CnzUSy283OYNLdUfu702quXtJOi.kZahYR9pIeY8g5N2ZD.r/kd8m','marko1','061234567','2025-04-05',2,1),(6,'Filip','Marković','filip@example.com','{bcrypt}$2a$10$jvD5Ewf8qhYs1bIAnjmIXezlxwKYXkSybH9RFY1KD.EEFkBqtg7Sa','filip','061234567','2025-04-05',NULL,1),(9,'Filip','Filipovic','filip2@example.com','{bcrypt}$2a$10$cmH/G0/ZglUoL0N4W4a6lOEi.0nLAkf060vEZNQRUT9VPBGChpWbO','filip1','067123123','2025-04-14',NULL,1);
+INSERT INTO `korisnik` VALUES (1,'Marko','Jocovic','marko@gmail.com','{bcrypt}$2a$10$DUcc/j4Rst5kP92J6Jhc3O3jYTMVBtnDNppVO6iTW9aSASBMoFcRO','Kesa','068051223','2025-05-06',NULL,1);
 /*!40000 ALTER TABLE `korisnik` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,7 +99,7 @@ CREATE TABLE `obavjestenja` (
   KEY `pitch_id` (`pitch_id`),
   CONSTRAINT `obavjestenja_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `korisnik` (`id`),
   CONSTRAINT `obavjestenja_ibfk_2` FOREIGN KEY (`pitch_id`) REFERENCES `teren` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,6 +108,7 @@ CREATE TABLE `obavjestenja` (
 
 LOCK TABLES `obavjestenja` WRITE;
 /*!40000 ALTER TABLE `obavjestenja` DISABLE KEYS */;
+INSERT INTO `obavjestenja` VALUES (1,1,1,'Tražimo jednog igrača za futsal','Fali nam jedan igrač za večerašnji meč u 18h, javite se!','2025-05-06',0),(2,1,2,'Košarka 2 na 2!','Ekipa iz City Kvarta traži protivnike za friendly 2v2.','2025-05-07',1);
 /*!40000 ALTER TABLE `obavjestenja` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,17 +126,14 @@ CREATE TABLE `ocjena` (
   `intensity` int DEFAULT NULL,
   `quality` int DEFAULT NULL,
   `team_id` int NOT NULL,
-  `loan_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `team_id` (`team_id`),
-  UNIQUE KEY `loan_id` (`loan_id`),
-  CONSTRAINT `loan_rating` FOREIGN KEY (`loan_id`) REFERENCES `pozajmica` (`id`),
   CONSTRAINT `team_rating` FOREIGN KEY (`team_id`) REFERENCES `ekipa` (`id`),
   CONSTRAINT `ocjena_chk_1` CHECK (((`overall` >= 0.0) and (`overall` <= 5.0))),
   CONSTRAINT `ocjena_chk_2` CHECK (((`fair_play` >= 0) and (`fair_play` <= 5))),
   CONSTRAINT `ocjena_chk_3` CHECK (((`intensity` >= 0) and (`intensity` <= 5))),
   CONSTRAINT `ocjena_chk_4` CHECK (((`quality` >= 0) and (`quality` <= 5)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,7 +142,42 @@ CREATE TABLE `ocjena` (
 
 LOCK TABLES `ocjena` WRITE;
 /*!40000 ALTER TABLE `ocjena` DISABLE KEYS */;
+INSERT INTO `ocjena` VALUES (1,4.7,4,5,5,1),(2,4.3,5,4,4,2);
 /*!40000 ALTER TABLE `ocjena` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ocjena_pozajmica`
+--
+
+DROP TABLE IF EXISTS `ocjena_pozajmica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ocjena_pozajmica` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `overall` decimal(2,1) DEFAULT NULL,
+  `fair_play` int DEFAULT NULL,
+  `intensity` int DEFAULT NULL,
+  `quality` int DEFAULT NULL,
+  `loan_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `loan_id` (`loan_id`),
+  CONSTRAINT `loan_rating` FOREIGN KEY (`loan_id`) REFERENCES `pozajmica` (`id`),
+  CONSTRAINT `ocjena_chk_loan_1` CHECK (((`overall` >= 0.0) and (`overall` <= 5.0))),
+  CONSTRAINT `ocjena_chk_loan_2` CHECK (((`fair_play` >= 0) and (`fair_play` <= 5))),
+  CONSTRAINT `ocjena_chk_loan_3` CHECK (((`intensity` >= 0) and (`intensity` <= 5))),
+  CONSTRAINT `ocjena_chk_loan_4` CHECK (((`quality` >= 0) and (`quality` <= 5)))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ocjena_pozajmica`
+--
+
+LOCK TABLES `ocjena_pozajmica` WRITE;
+/*!40000 ALTER TABLE `ocjena_pozajmica` DISABLE KEYS */;
+INSERT INTO `ocjena_pozajmica` VALUES (1,5.0,5,5,5,1),(2,4.0,4,4,4,2);
+/*!40000 ALTER TABLE `ocjena_pozajmica` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -164,7 +197,7 @@ CREATE TABLE `pozajmica` (
   KEY `reservation_id` (`reservation_id`),
   CONSTRAINT `pozajmica_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `korisnik` (`id`),
   CONSTRAINT `pozajmica_ibfk_2` FOREIGN KEY (`reservation_id`) REFERENCES `rezervacija` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,6 +206,7 @@ CREATE TABLE `pozajmica` (
 
 LOCK TABLES `pozajmica` WRITE;
 /*!40000 ALTER TABLE `pozajmica` DISABLE KEYS */;
+INSERT INTO `pozajmica` VALUES (1,1,1,5.0),(2,1,2,4.0);
 /*!40000 ALTER TABLE `pozajmica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,7 +229,7 @@ CREATE TABLE `rezervacija` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `rezervacija_ibfk_1` FOREIGN KEY (`pitch_id`) REFERENCES `teren` (`id`),
   CONSTRAINT `rezervacija_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `korisnik` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,6 +238,7 @@ CREATE TABLE `rezervacija` (
 
 LOCK TABLES `rezervacija` WRITE;
 /*!40000 ALTER TABLE `rezervacija` DISABLE KEYS */;
+INSERT INTO `rezervacija` VALUES (1,'zauzeto','2025-05-10 18:00:00','2025-05-10 19:00:00',1,1),(2,'zauzeto','2025-05-11 20:00:00','2025-05-11 21:00:00',2,1),(3,'slobodno','2025-05-12 17:00:00','2025-05-12 18:00:00',3,NULL);
 /*!40000 ALTER TABLE `rezervacija` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -228,7 +263,7 @@ CREATE TABLE `teren` (
   KEY `owner_of_pitch` (`owner_id`),
   CONSTRAINT `owner_of_pitch` FOREIGN KEY (`owner_id`) REFERENCES `korisnik` (`id`),
   CONSTRAINT `chk_rating` CHECK (((`rating` >= 0.0) and (`rating` <= 5.0)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,6 +272,7 @@ CREATE TABLE `teren` (
 
 LOCK TABLES `teren` WRITE;
 /*!40000 ALTER TABLE `teren` DISABLE KEYS */;
+INSERT INTO `teren` VALUES (1,'Morača - Futsal teren','Bulevar Revolucije bb','067123456',1,'Futsal teren sa veštačkom travom, osvetljenjem i svlačionicama.',4.5,1),(2,'Tološi - Košarkaški teren','Ulica V Proleterske','067987654',2,'Otvoreni košarkaški teren sa reflektorima.',4.2,1),(3,'Teniski tereni Ljubović','Ljubović bb','068222333',3,'Dva teniska terena sa tartan podlogom.',4.7,1);
 /*!40000 ALTER TABLE `teren` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -273,4 +309,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-16 23:00:26
+-- Dump completed on 2025-05-06 13:31:26
