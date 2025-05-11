fc55dfe4a6395ece40981a5b58b562a6365bd632
Filip <filipzejak2@gmail.com>
2025-05-11 12:05:23 +0200

    added chatbot

--- frontend/rekreacija/src/app/components/app/app.component.ts
+++ frontend/rekreacija/src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild, HostListener, viewChild, ElementRef} from '@angular/core';
 import {AuthModalComponent} from "../../modals/auth-modal/auth-modal.component";
 import {AuthService} from "../../services/auth.service";
 
@@ -12,6 +12,10 @@ export class AppComponent implements AfterViewInit {
   title = 'rekreacija';
 
   @ViewChild(AuthModalComponent, { static: false}) authModal!: AuthModalComponent;
+  @ViewChild('chatbotIframe') chatbotIframe!: ElementRef;
+  @ViewChild('chatbotButton') chatbotButton!: ElementRef;
+
+  isChatbotVisible = false;
 
   constructor(private authService: AuthService) {}
 
@@ -20,4 +24,18 @@ export class AppComponent implements AfterViewInit {
         this.authModal.openModal(resolve);
       };
   };
+
+  toggleChatbot() {
+    this.isChatbotVisible = !this.isChatbotVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInsideIframe = this.chatbotIframe?.nativeElement.contains(event.target);
+    const clickedOnButton = this.chatbotButton?.nativeElement.contains(event.target);
+
+    if(this.isChatbotVisible && !clickedInsideIframe && !clickedOnButton) {
+      this.isChatbotVisible = false;
+    }
+  }
 }
