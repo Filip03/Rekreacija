a404ffa583e162e8255aae0e8590f59828bf8cea
tovi21 <tovjanina@gmail.com>
2025-07-01 15:12:26 +0200

    finished tereni page

--- frontend/rekreacija/src/app/services/teren.service.ts
+++ frontend/rekreacija/src/app/services/teren.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Teren } from '../models/teren.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,9 @@ import { environment } from '../environments/environment';
 export class TerenService {
   private apiUrl = `${environment.API_URL}/api/teren`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTereni(): Observable<Teren[]> {
+    return this.http.get<Teren[]>(this.apiUrl);
+  }
 }
