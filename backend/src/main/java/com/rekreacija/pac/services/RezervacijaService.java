3ed6be5e35b52c7b2a4d1e203a6895a91e5cc421
Filip <filipzejak2@gmail.com>
2025-07-02 13:44:49 +0200

    added verification before inserting new Reservation, returns 409 if appointment is already taken

--- backend/src/main/java/com/rekreacija/pac/services/RezervacijaService.java
+++ backend/src/main/java/com/rekreacija/pac/services/RezervacijaService.java
@@ -2,8 +2,11 @@ package com.rekreacija.pac.services;
 
 import com.rekreacija.pac.models.Rezervacija;
 import com.rekreacija.pac.repositories.RezervacijaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +24,13 @@ public class RezervacijaService {
     }
     public List<Rezervacija> getRezervacijaByUserId(int id) { return this.rezervacijaRepository.getRezervacijaByUserId(id); }
     public int insertRezervacija(Rezervacija rez) {
+
+        List<Rezervacija> all = this.rezervacijaRepository.getAllRezervacija();
+        for(Rezervacija r : all) {
+            if(r.pitch_id == rez.pitch_id && rez.start_date.isBefore(r.end_date) && rez.end_date.isAfter(r.start_date)) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Termin je zauzet!");
+            }
+        }
         return this.rezervacijaRepository.insertRezervacija(rez);
     }
     public int updateRezervacija(Rezervacija rezervacija, int rezervacija_id) {
